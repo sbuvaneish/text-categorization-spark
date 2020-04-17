76ac06ff3082aeaf7e5e6e2576ecc82a8a67b1cb
buvaneish <sbuvaneish@gmail.com>
2020-04-17 12:32:53 -0500

    post_script added to ec2 copier

--- ec2_copier.sh
+++ ec2_copier.sh
@@ -1,3 +1,4 @@
 scp -i ../sbh-cloudproject3-ec2-emr-key-pair.pem ../sbh-cloudproject3-ec2-emr-key-pair.pem "$1":~
 scp -i ../sbh-cloudproject3-ec2-emr-key-pair.pem launch_predictor.sh "$1":~
 scp -i ../sbh-cloudproject3-ec2-emr-key-pair.pem bootstrap.sh "$1":~
+scp -i ../sbh-cloudproject3-ec2-emr-key-pair.pem post_script.py "$1":~
