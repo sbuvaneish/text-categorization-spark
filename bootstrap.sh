92039af30a86461a481884cdb3eebeffbada58a5
buvaneish <sbuvaneish@gmail.com>
2020-04-16 20:20:27 -0500

    changes to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,5 @@ python3 get-pip.py --user
 echo "export PATH=~/.local/bin:$PATH" >> ~/.bash_profile
 source ~/.bash_profile
 export PYSPARK_PYTHON=/usr/bin/python3
-export PYSPARK_DRIVER_PYTHON=jupyter
-export PYSPARK_DRIVER_PYTHON_OPTS="notebook"
-sudo pip install keras
-sudo pip install boto3
-
+pip install keras
+pip install boto3
