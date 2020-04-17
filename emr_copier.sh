9b3c9dcbb0236ebc87dbfca5bdeb7eddedc839be
buvaneish <sbuvaneish@gmail.com>
2020-04-16 20:07:03 -0500

    Change to emr_copier

--- emr_copier.sh
+++ emr_copier.sh
@@ -1,4 +1,4 @@
 scp -i ../sbh-cloudproject3-ec2-emr-key-pair.pem bootstrap.sh "$1":~
-scp -i ../sbh-cloudproject3-ec2-emr-key-pair.pem prediction "$1":~
+scp -i ../sbh-cloudproject3-ec2-emr-key-pair.pem prediction.py "$1":~
 ssh -i ../sbh-cloudproject3-ec2-emr-key-pair.pem "$1" "bash ~/bootstrap.sh"
 exit
